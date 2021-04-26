c0aa966827373d7b8dd7277dead7eda226b67cd0
abhishekiiitp <77091113+abhishekiiitp@users.noreply.github.com>
2021-04-26 19:05:37 +0530

    Updates

---
+++ precache-manifest.6c73f4d0c02b397795f4bbd9b8b4477f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1e3e4149e770a5b0cc0e534efff2bf5",
+    "url": "/Codingso-using-react/index.html"
+  },
+  {
+    "revision": "2ac5e7ffc73930e26b60",
+    "url": "/Codingso-using-react/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "95a39d914dcf93aec1bf",
+    "url": "/Codingso-using-react/static/css/main.7a815947.chunk.css"
+  },
+  {
+    "revision": "2ac5e7ffc73930e26b60",
+    "url": "/Codingso-using-react/static/js/2.670fdb07.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/Codingso-using-react/static/js/2.670fdb07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95a39d914dcf93aec1bf",
+    "url": "/Codingso-using-react/static/js/main.dbe42c69.chunk.js"
+  },
+  {
+    "revision": "30394d49b9b2dc910c67",
+    "url": "/Codingso-using-react/static/js/runtime-main.1933b23e.js"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/Codingso-using-react/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/Codingso-using-react/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/Codingso-using-react/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/Codingso-using-react/static/media/hero-img.619f0cc8.png"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/Codingso-using-react/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/Codingso-using-react/static/media/marketing.2eed95a2.jpg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/Codingso-using-react/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/Codingso-using-react/static/media/software.43049cae.jpeg"
+  }
+]);
